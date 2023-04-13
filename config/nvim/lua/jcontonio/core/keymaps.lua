a0ac12079a476df849153d5d3e1a2bba3337c8ad
Jay Contonio <jay@conton.io>
2023-04-13 11:22:15 -0700

    updating keymaps

--- config/nvim/lua/jcontonio/core/keymaps.lua
+++ config/nvim/lua/jcontonio/core/keymaps.lua
@@ -18,7 +18,7 @@ keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
 -- tabs
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab 
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previos tab
@@ -37,3 +37,6 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buggers<cr>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+-- diagnostic
+keymap.set("n", "<leader>dd", "<cmd>lua vim.diagnostic.open_float()<cr>")

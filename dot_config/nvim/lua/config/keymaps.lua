1f214944651edd5f3d249d620c02954cf5ac6d02
Jay Contonio <jay@conton.io>
2025-11-18 09:42:13 -0700

    Updating themes

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Make gd jump to definition in the current window
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })

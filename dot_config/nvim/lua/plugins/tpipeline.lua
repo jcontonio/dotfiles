344603b22dc6a627f5ab3f41dfff45b23e5925a8
Jay Contonio <jay@conton.io>
2026-02-25 10:42:41 -0500

    Adjusting nvim settings

--- dot_config/nvim/lua/plugins/tpipeline.lua
+++ dot_config/nvim/lua/plugins/tpipeline.lua
@@ -10,5 +10,16 @@ return {
       -- Avoid stacked Neovim + tmux statuslines.
       vim.g.tpipeline_clearstl = 1
     end,
+    config = function()
+      -- In tmux, hide Neovim's own statusline line and let tpipeline render
+      -- the status into tmux's bar instead.
+      local group = vim.api.nvim_create_augroup("tpipeline_statusline", { clear = true })
+      vim.api.nvim_create_autocmd({ "VimEnter", "UIEnter" }, {
+        group = group,
+        callback = function()
+          vim.opt.laststatus = 0
+        end,
+      })
+    end,
   },
 }
